ef7aa09ccf40eb16c075ef94e9ce47741c6fcdff
RahmaMa <ramamacina96@hotmail.com>
2020-07-07 17:29:14 +0200

    "dernier commit du à un bug du pc"

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const Livres = require('./models/pizza') // on importe notre model
+const pizza = require('./models/pizza') // on importe notre model
+const body = require('body-parser')
+app.use(body())
 let app = express(); // creation app server
 let port = 3000;   // le port donné
-mongoose.connect('mongodb://localhost:27017/livres', {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost:3000/gestionPizza', {useNewUrlParser: true});
 
 app.get('/', function(req, res) {     // nous permet d'avoir la route par defaut
     res.send('Hello world !')});
@@ -15,5 +18,39 @@ app.get('/Ingredients',async (req, res)=> {const ingredients = await ingredients
 
 app.listen(port, () =>  { // ecoute du serveur sur le port 3000
     console.log('demarrage du serveur')
-})
+});
+// la connexion avec le fichier index.html
+var express = require('express');
+var app = express();
+var path = require('path');
 
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(300);
+
+app.post('/', async (req, res) => {
+    const nom = req.body.nom; // récupération des variables du body
+    const description = req.body.description
+    const ingredients = req.body.ingredients
+    const quantiteIngredients = req.body.quantiteIngredients
+
+
+    if (!nom || ! description || !ingrédients) { // on vérifie que les trois variables sont présentes
+        res.send('Il manque un argument')
+        return
+    }
+
+    const newRecette= new Recettes({ // création d'un objet représentant notre nouvelle recette
+        nom: nom,
+        description : description ,
+        ingrédients: ingrédients
+    })
+
+    await newRecette.save() // sauvegarde asynchrone du nouveau livre
+    res.json(newRecette)
+    return
+
+});
